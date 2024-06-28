bc8a3f92419999ad1e1d15a7250719a884494887
sarxxt <sararasool2002@gmail.com>
2024-06-29 00:09:23 +0500

    modified arg

--- str_fun.py
+++ str_fun.py
@@ -1,12 +1,10 @@
 class Person:
-    def __init__(self, name, age):
-        self.name = name
+    def __init__(self, name, age): #  The self parameter is a 
+        self.name = name           #  reference to the current instance of the class, and is used to access variables that belong to the class.
         self.age = age
 
     def __str__(self):
         return f"{self.name}({self.age})"
     
-p1=Person("sara", 21)
+p1=Person("sara", 221)
 print(p1)    
-
-    
